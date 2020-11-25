001d7c2adf1fa6d62cc94bcdfad6f802a8f57b00
hadarpl <50652447+hadarpl@users.noreply.github.com>
2020-11-25 11:57:59 +0200

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -47,7 +47,7 @@ resource "aws_s3_bucket" "operations" {
   # bucket is not encrypted
   # bucket does not have access logs
   bucket = "${local.resource_prefix.value}-operations"
-  acl    = "private"
+  #acl    = "private"
   versioning {
     enabled = true
   }
