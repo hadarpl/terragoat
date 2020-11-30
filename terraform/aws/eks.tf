917145f9d9cfb205e614571089b3571c1bf81bb1
hadarpl <50652447+hadarpl@users.noreply.github.com>
2020-11-30 19:27:48 +0200

    Update eks.tf

--- terraform/aws/eks.tf
+++ terraform/aws/eks.tf
@@ -16,7 +16,7 @@ data aws_iam_policy_document "iam_policy_eks" {
 }
 
 resource aws_iam_role "iam_for_eks" {
-  name               = "${local.resource_prefix.value}-iam-for-eks"
+  name               = "${local.resource_prefix.value}-iam-for-eks-1"
   assume_role_policy = data.aws_iam_policy_document.iam_policy_eks.json
 }
 
